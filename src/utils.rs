17238af18bb58475fcbb27762de8864480116fae
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2024-01-24 21:22:05 +0100

    feat(cli): add util to check if any file in list exists

--- src/utils.rs
+++ src/utils.rs
@@ -12,8 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use anyhow::Result;
 use chrono::{Datelike, TimeZone};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+  fs::File,
+  io::Write,
+  path::{Path, PathBuf},
+  time::{SystemTime, UNIX_EPOCH},
+};
 
 /// Returns the current year as determined by the OS.
 ///
@@ -30,9 +36,44 @@ pub fn current_year() -> u16 {
     .unwrap()
     .year() as u16
 }
+
+/// Checks if any of the specified filenames exist in the given path.
+///
+/// # Arguments
+///
+/// * `path` - The base path where the function checks for the existence of the specified files.
+/// * `filenames` - A slice of strings representing the filenames to check for existence.
+///
+/// # Returns
+///
+/// Returns an `Option<PathBuf>` representing the path of the first existing file, if any.
+/// Returns `None` if none of the specified files exist in the given path.
+///
+/// # Panics
+///
+/// This function does not intentionally panic
+pub fn check_any_file_exists<P>(path: P, filenames: &[&str]) -> Option<PathBuf>
+where
+  P: AsRef<Path>,
+{
+  let mut out_path: Option<PathBuf> = None;
+  filenames.iter().for_each(|filename: &&str| {
+    let file_path = path.as_ref().join(filename);
+    if file_path.exists() {
+      let _ = out_path.insert(file_path);
+    }
+  });
+
+  out_path
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
+  use std::fs::File;
+  use std::io::{Read, Seek, SeekFrom};
+  use tempfile::tempdir;
+
   #[test]
   fn test_current_year() {
     // Test the current_year function
@@ -45,4 +86,63 @@ mod tests {
     assert_eq!(current_year, chrono_current_year);
   }
 
+  #[test]
+  fn test_check_any_file_exists_single_file_exists() {
+    // Create a temporary directory for testing
+    let temp_dir = tempdir().expect("Failed to create temporary directory");
+    let base_path = temp_dir.path();
+
+    // Create a sample file in the temporary directory
+    let sample_filename = "file1.txt";
+    let sample_file_path = base_path.join(sample_filename);
+    File::create(&sample_file_path).expect("Failed to create sample file");
+
+    // Test when the single file exists
+    let result = check_any_file_exists(base_path, &[sample_filename]);
+    assert_eq!(result, Some(sample_file_path.clone()));
+
+    // Cleanup
+    drop(sample_file_path);
+    temp_dir.close().expect("Failed to close temp directory");
+  }
+
+  #[test]
+  fn test_check_any_file_exists_multiple_files_exist() {
+    // Create a temporary directory for testing
+    let temp_dir = tempdir().expect("Failed to create temporary directory");
+    let base_path = temp_dir.path();
+
+    // Create sample files in the temporary directory
+    let filenames = ["file1.txt", "file2.txt", "file3.txt"];
+    for &filename in &filenames {
+      let file_path = base_path.join(filename);
+      File::create(&file_path).expect("Failed to create sample file");
+
+      // Cleanup
+      drop(file_path);
+    }
+
+    // Test when multiple files exist
+    let result = check_any_file_exists(base_path, &filenames);
+    assert!(result.is_some());
+    assert!(filenames.iter().any(|&filename| {
+      result
+        .as_ref()
+        .map_or(false, |path| path.ends_with(filename))
+    }));
+
+    // Cleanup
+    temp_dir.close().expect("Failed to close temp directory");
+  }
+
+  #[test]
+  fn test_check_any_file_exists_no_file_exists() {
+    // Create a temporary directory for testing
+    let temp_dir = tempdir().expect("Failed to create temporary directory");
+    let base_path = temp_dir.path();
+
+    // Test when none of the files exist
+    let result = check_any_file_exists(base_path, &["nonexistent_file.txt"]);
+    assert_eq!(result, None);
+  }
 }
