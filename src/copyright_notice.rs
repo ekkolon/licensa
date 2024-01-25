6dad17c48bc14bb36caa8ac00e2ee84dc2121fe9
Nelson Dominguez <ekkolon@proton.me>
2024-01-25 04:36:52 +0100

    refactor(cli): define CompactCopyrightNotice defaults in impl block

--- src/copyright_notice.rs
+++ src/copyright_notice.rs
@@ -25,18 +25,22 @@ pub struct CompactCopyrightNotice {
   pub year: u16,
 
   /// A word indicating where to find the LICENSE file (e.g., "in").
-  #[serde(default = "default_determiner")]
+  #[serde(default = "CompactCopyrightNotice::default_determiner")]
   pub determiner: String,
 
   /// The location where the LICENSE file can be found.
-  #[serde(default = "default_location")]
+  #[serde(default = "CompactCopyrightNotice::default_location")]
   pub location: String,
 }
 
-fn default_determiner() -> String {
-  "in".to_string()
+impl CompactCopyrightNotice {
+  fn default_determiner() -> String {
+    "in".to_string()
+  }
+
+  fn default_location() -> String {
+    "the root of this project".to_string()
+  }
 }
 
-fn default_location() -> String {
-  "the root of this project".to_string()
 }
