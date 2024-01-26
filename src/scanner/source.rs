5f0fd9b4a7519d3b49d58bb04b13331a6703f35e
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2024-01-26 15:22:50 +0100

    docs(cli): add rustdocs to scanner source mod

--- src/scanner/source.rs
+++ src/scanner/source.rs
@@ -1,9 +1,42 @@
 // Copyright 2024 Nelson Dominguez
 // SPDX-License-Identifier: Apache-2.0
 
+//! # Source Headers
+//!
+//! The `SourceHeaders` struct provides methods for finding header definitions and prefixes based on file extensions.
+//! It contains a predefined list of `SourceHeaderDefinition` instances.
+//!
+//! # Example
+//!
+//! ```no_run
+//! use licensa::scanner::SourceHeaders;
+//!
+//! let extension = ".rs";
+//! if let Some(header_prefix) = SourceHeaders::find_header_prefix_for_extension(extension) {
+//!     println!("Header Prefix: {}", header_prefix.top);
+//! }
+//! ```
+//!
+//! # Source Header Definitions
+//!
+//! Each source header definition includes a list of file extensions and a corresponding `SourceHeaderPrefix`.
+//!
+//! # Example
+//!
+//! ```no_run
+//! use licensa::scanner::{SourceHeaders, SourceHeaderPrefix};
+//!
+//! let header_definition = SourceHeaders::find_header_definition_by_extension(".rs");
+//! if let Some(header_def) = header_definition {
+//!     println!("Extensions: {:?}", header_def.extensions);
+//!     println!("Header Prefix: {}", header_def.header_prefix.top);
+//! }
+//! ```
+
 use lazy_static::lazy_static;
 
 lazy_static! {
+  /// Represents a predefined list of source header definitions.
   static ref SOURCE_HEADER_DEFINITIONS: Vec<SourceHeaderDefinition<'static>> = vec![
     SourceHeaderDefinition {
       extensions: vec![".c", ".h", ".gv", ".java", ".scala", ".kt", ".kts"],
@@ -70,10 +103,22 @@ lazy_static! {
     // TODO: 	handle cmake files
   ];
 }
-
+/// Represents a utility for working with source headers.
 pub struct SourceHeaders;
 
 impl SourceHeaders {
+  /// Finds the header definition based on the given file extension.
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// use licensa::scanner::SourceHeaders;
+  ///
+  /// let extension = ".rs";
+  /// if let Some(header_definition) = SourceHeaders::find_header_definition_by_extension(extension) {
+  ///     // Do something with the header definition
+  /// }
+  /// ```
   pub fn find_header_definition_by_extension<'a, E: AsRef<str>>(
     extension: E,
   ) -> Option<&'a SourceHeaderDefinition<'a>> {
@@ -82,6 +127,18 @@ impl SourceHeaders {
       .find(|source| source.contains_extension(Some(&extension)))
   }
 
+  /// Finds the header prefix based on the given file extension.
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// use licensa::scanner::SourceHeaders;
+  ///
+  /// let extension = ".rs";
+  /// if let Some(header_prefix) = SourceHeaders::find_header_prefix_for_extension(extension) {
+  ///     // Do something with the header prefix
+  /// }
+  /// ```
   pub fn find_header_prefix_for_extension<'a, E: AsRef<str>>(
     extension: E,
   ) -> Option<&'a SourceHeaderPrefix<'a>> {
@@ -90,12 +147,30 @@ impl SourceHeaders {
   }
 }
 
+/// Represents a source header definition with a list of file extensions and a corresponding prefix.
 pub struct SourceHeaderDefinition<'a> {
+  /// List of file extensions associated with the header definition.
   pub extensions: Vec<&'a str>,
+  /// Corresponding source header prefix.
   pub header_prefix: SourceHeaderPrefix<'a>,
 }
 
 impl<'a> SourceHeaderDefinition<'a> {
+  /// Checks if the given extension is contained in the list of file extensions.
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// use licensa::scanner::SourceHeaderDefinition;
+  ///
+  /// let header_def = SourceHeaderDefinition {
+  ///     extensions: vec![".rs", ".toml"],
+  ///     header_prefix: Default::default(),
+  /// };
+  ///
+  /// let is_contained = header_def.contains_extension(Some(".rs"));
+  /// assert_eq!(is_contained, true);
+  /// ```
   pub fn contains_extension<E: AsRef<str>>(&self, extension: Option<E>) -> bool {
     extension
       .map_or(false, |e| self.extensions.contains(&e.as_ref()))
@@ -103,14 +178,27 @@ impl<'a> SourceHeaderDefinition<'a> {
   }
 }
 
+/// Represents the prefix structure for a source header.
 #[derive(Debug, Clone)]
 pub struct SourceHeaderPrefix<'a> {
+  /// Top part of the header.
   pub top: &'a str,
+  /// Middle part of the header.
   pub mid: &'a str,
+  /// Bottom part of the header.
   pub bottom: &'a str,
 }
 
 impl<'a> SourceHeaderPrefix<'a> {
+  /// Creates a new `SourceHeaderPrefix` instance with the specified top, mid, and bottom parts.
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// use licensa::scanner::SourceHeaderPrefix;
+  ///
+  /// let header_prefix = SourceHeaderPrefix::new("/**", " * ", " */");
+  /// ```
   pub fn new(top: &'a str, mid: &'a str, bottom: &'a str) -> SourceHeaderPrefix<'a> {
     SourceHeaderPrefix { top, mid, bottom }
   }
