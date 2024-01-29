b919138bd530629bf4d1c8ea1776920b7a36cd4e
Nelson Dominguez <ekkolon@proton.me>
2024-01-29 10:51:45 +0100

    feat(cli): round duration to decimal places

--- src/helpers/channel_duration.rs
+++ src/helpers/channel_duration.rs
@@ -25,4 +25,17 @@ impl ChannelDuration {
             Some(dropped_time) => dropped_time.duration_since(self.creation_time).unwrap(),
         }
     }
+
+    pub fn as_secs_rounded(&self) -> f32 {
+        // Multiply the number by 100 to get two decimal places
+        let mut rounded_number = self.get_duration().as_secs_f32() * 100.0;
+
+        // Floor the number to the nearest integer
+        rounded_number = f32::floor(rounded_number);
+
+        // Divide the rounded number by 100 to get the rounded decimal part
+        rounded_number /= 100.0;
+
+        rounded_number
+    }
 }
