4851ddc0817d99d8d94799eb8afdac8480b6a911
Nelson Dominguez <ekkolon@proton.me>
2024-01-30 18:22:04 +0100

    feat(cli): add basic `init` subcommand functionality (#17)
    * feat(cli): add `init` subcommand base
    
    * refactor(cli): remove obsolete logger

--- src/logger.rs
+++ src/logger.rs
@@ -3,26 +3,6 @@
 
 #![allow(unused_macros)]
 
-use chrono::Local;
-use env_logger::Builder;
-use log::LevelFilter;
-use std::io::Write;
-
-pub fn init() {
-    Builder::new()
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "{} [{}] - {}",
-                Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                record.level(),
-                record.args()
-            )
-        })
-        .filter(None, LevelFilter::Info)
-        .init();
-}
-
 macro_rules! notice {
     ($message:expr) => {
         let level = "INFO".blue().bold();
