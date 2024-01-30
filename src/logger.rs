c52ec16204928306c1c3ec9a9b4081666d812868
Nelson Dominguez <ekkolon@proton.me>
2024-01-30 20:04:24 +0100

    feat(cli): improve Licensa config struct  (#18)
    * feat(cli): add `init` subcommand base
    
    * refactor(cli): remove obsolete logger
    
    * refactor(cli): expose `failure` logging macro
    
    * feat(cli): refine Config struct to better reflect license metadata
    
    * feat(cli): add a builder for Config struct

--- src/logger.rs
+++ src/logger.rs
@@ -1,7 +1,7 @@
 // Copyright 2024 Nelson Dominguez
 // SPDX-License-Identifier: Apache-2.0
 
-#![allow(unused_macros)]
+#![allow(unused_macros, unused_imports)]
 
 macro_rules! notice {
     ($message:expr) => {
@@ -25,7 +25,7 @@ macro_rules! failure {
         println!("{level}: {}", $message);
     };
 }
-// pub(crate) use failure;
+pub(crate) use failure;
 
 macro_rules! warning {
     ($message:expr) => {
