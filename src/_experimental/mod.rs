a12e4c5960f8502c177da1bd8f04724788b5eb8b
Nelson Dominguez <ekkolon@proton.me>
2024-02-03 08:07:00 +0100

    refactor: prepare main branch for distribution (#29)
    * refactor: remove _experimental/ directory
    
    The contents of this directory is still available in the `experimental` branch
    
    * chore(repo): license under either of MIT or Apache
    
    * refactor: flatten cli mod
    
    * chore(repo): update SPDX license identifier
    
    * docs: update work_tree mod docs

--- src/_experimental/mod.rs
+++
@@ -1,2 +0,0 @@
-// Copyright 2024 Nelson Dominguez
-// SPDX-License-Identifier: Apache-2.0
--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
 // Copyright 2024 Nelson Dominguez
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 use crate::cli::Cli;
 use clap::CommandFactory;
