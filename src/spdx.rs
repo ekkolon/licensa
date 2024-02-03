3de7cf2d2793f6d7007c1239ac07672c891e66cd
Nelson Dominguez <ekkolon@proton.me>
2024-02-03 12:35:26 +0100

    feat: return spdx license search result struct

--- src/spdx.rs
+++ src/spdx.rs
@@ -1,21 +1,25 @@
 // Copyright 2024 Nelson Dominguez
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use spdx::{imprecise_license_id, license_id, Expression, LicenseId, ParseMode};
+use spdx::{imprecise_license_id, license_id, Expression, LicenseId, LicenseItem, ParseMode};
 
-pub fn try_find_by_id<I>(id: I) -> Option<LicenseId>
+pub struct SpdxSearchResult {
+    license_id: LicenseId,
+    single: bool,
+}
+
+pub fn try_find_by_id<I>(expr: I) -> Option<SpdxSearchResult>
 where
     I: AsRef<str>,
 {
-    let license = Expression::parse_mode(id.as_ref(), ParseMode::LAX);
-    if let Err(err) = license {
-        if let Some((license_id, _)) = imprecise_license_id(id.as_ref()) {
-            return Some(license_id);
-        }
-        return None;
-    }
-    let license = license.unwrap();
+    let license = Expression::parse_mode(expr.as_ref(), ParseMode::LAX).ok()?;
+    let requirements: Vec<&LicenseItem> = license.requirements().map(|r| &r.req.license).collect();
     license_id(license.as_ref())
+        .or_else(|| imprecise_license_id(expr.as_ref()).map(|(license_id, _)| license_id))
+        .map(|l| SpdxSearchResult {
+            license_id: l,
+            single: requirements.len() == 1,
+        })
 }
 
 #[cfg(test)]
@@ -24,8 +28,22 @@ mod tests {
 
     #[test]
     fn test_try_find_by_id_exact() {
-        let license_exp = "BSD-3-Clause";
-        let license_id = try_find_by_id(&license_exp);
+        let expr = "BSD-3-Clause";
+        let license_id = try_find_by_id(expr);
+        assert!(&license_id.is_some());
+    }
+
+    #[test]
+    fn test_try_find_by_id_invalid() {
+        let expr = "BSE";
+        let license_id = try_find_by_id(expr);
+        assert!(&license_id.is_none());
+    }
+
+    #[test]
+    fn test_try_find_by_id_combined() {
+        let expr = "mit or apache";
+        let license_id = try_find_by_id(expr);
         assert!(&license_id.is_some());
     }
 }
