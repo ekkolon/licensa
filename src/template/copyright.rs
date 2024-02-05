c480622d58b54119834afb9260df7e47b9c57926
Nelson Dominguez <ekkolon@proton.me>
2024-02-05 13:55:39 +0100

    fix: inconsistent SPDX header template interpolation (#34)
    * refactor: remove println statement in trait implementation
    
    * build(deps): add handlebars crate
    
    * feat: use handlebars to interpolate SPDX template
    
    * test: SPDX template interpolation
    
    * refactor: use proper names & types in SpdxCopyrightNotice struct
    
    * test: reflect interpolation changes

--- src/template/copyright.rs
+++ src/template/copyright.rs
@@ -2,31 +2,71 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use super::interpolation::{interpolate, Interpolate};
-use crate::utils::current_year;
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 /// Represents a simple SPDX copyright notice.
-pub const SPDX_COPYRIGHT_NOTICE: &str = r#"Copyright $(year) $(fullname)
-SPDX-License-Identifier: $(license)"#;
+pub const SPDX_COPYRIGHT_NOTICE: &str = r#"Copyright{{#if year}} {{year}}{{/if}} {{owner}}
+SPDX-License-Identifier: {{license}}"#;
 
 /// Holds information for a simple SPDX copyright notice.
 #[derive(Debug, Deserialize, Serialize, Default)]
 pub struct SpdxCopyrightNotice {
     /// The full name of the copyright holder.
-    pub fullname: String,
+    pub owner: String,
 
     /// The type of license governing the use of the source code.
     pub license: String,
 
     /// The year(s) to be included in the copyright notice.
-    #[serde(default = "current_year")]
-    pub year: u16,
+    pub year: Option<u16>,
 }
 
 impl Interpolate for SpdxCopyrightNotice {
     fn interpolate(&self) -> Result<String> {
         interpolate!(SPDX_COPYRIGHT_NOTICE, &self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_spdx_template_without_year() {
+        let reg = handlebars::Handlebars::new();
+
+        let data = json!({
+            "owner": "Bilbo Baggins",
+            "license": "MIT"
+        });
+
+        let expected_template = "Copyright Bilbo Baggins\nSPDX-License-Identifier: MIT";
+
+        let template = reg.render_template(SPDX_COPYRIGHT_NOTICE, &data);
+
+        assert!(template.is_ok());
+        assert_eq!(template.unwrap(), expected_template.to_string());
+    }
+
+    #[test]
+    fn test_spdx_template_with_year() {
+        let reg = handlebars::Handlebars::new();
+
+        let data = json!({
+            "owner": "Gandalf",
+            "license": "MIT OR Apache-2.0",
+            "year": 2012
+        });
+
+        let expected_template =
+            "Copyright 2012 Gandalf\nSPDX-License-Identifier: MIT OR Apache-2.0";
+
+        let template = reg.render_template(SPDX_COPYRIGHT_NOTICE, &data);
+
+        assert!(template.is_ok());
+        assert_eq!(template.unwrap(), expected_template.to_string());
     }
 }
