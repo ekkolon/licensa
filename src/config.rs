0eaea73307905064a5ed06e3479c711fa62d063f
Nelson Dominguez <ekkolon@proton.me>
2024-02-11 05:43:09 +0100

    docs: improve Config struct rustdocs

--- src/config.rs
+++ src/config.rs
@@ -31,49 +31,69 @@ pub const LICENSA_CONFIG_FILENAME: &str = ".licensarc";
 /// in the config file. An exeception to that rule is when a command
 /// accepts a `--config` flag, which, when present, explicitly requests
 /// the usage of a specific Licensa config file.
-///
-/// It is assumed the file is in valid JSON format and is named after one
-/// of the following filenames:
-///
-///   - `.licensarc`
-///   - `.licensarc.json`
 #[derive(Debug, Clone, Default, Serialize, Deserialize, Args)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields, default)]
 pub struct Config {
-    /// The SPDX identifier of the license (case-insensitive).
+    /// The SPDX license ID or expression (case-insensitive).
     ///
-    /// SPDX (Software Package Data Exchange) is a standard format for communicating the components,
-    /// licenses, and copyrights associated with software.
+    /// If a value partially matches a SPDX license ID, it is automatically transformed
+    /// into the most likely SPDX license expression match. For example, "apache" would
+    /// become "Apache-2.0", "mits" is transformed into "MIT" and so on.
+    /// However, an error is thrown if no match is found for the imprecise expression.
     ///
-    /// See https://spdx.org/licenses/
-    #[arg(short = 't', long = "type", value_parser = crate::parser::parse_license_id)]
+    /// For a comprehensive list of the available SPDX refer to https://spdx.org/licenses/.
+    #[arg(short = 't', long = "type", verbatim_doc_comment)]
+    #[arg(value_name = "ID")]
+    #[arg(value_parser = crate::parser::parse_license_id)]
     pub license: Option<LicenseId>,
 
     /// The copyright owner.
-    #[arg(short, long, value_name = "NAME")]
+    #[arg(short, long, verbatim_doc_comment, value_name = "NAME")]
     pub owner: Option<String>,
 
     /// Represents the copyright year or a range of years.
     ///
-    /// The `year` field accepts various formats, allowing flexibility in specifying the copyright period:
-    ///
-    /// - A single year, e.g., `2022`.
-    ///
-    /// - A range of years, e.g., `2022-2024`.
-    ///
-    /// - The special keyword `present`, indicating the current year, e.g., `2022-present`.
-    ///
     /// This field is used to define the copyright duration when applying license headers.
     /// When providing a range, it signifies the inclusive span of years.
-    #[arg(long, value_name = "YYYY | YYYY-YYYY | YYYY-present", value_parser = crate::parser::parse_license_year)]
+    ///
+    /// The special keyword `present` indicates the current year, e.g. "2022-present".
+    ///
+    /// === EXAMPLE USAGE ================================================
+    ///     
+    ///     licensa <COMMAND> --year 2020
+    ///     licensa <COMMAND> --year 2020-2023
+    ///     licensa <COMMAND> --year 2020-present
+    #[cfg(not(doctest))]
+    #[arg(long, verbatim_doc_comment)]
+    #[arg(value_name = "YEAR | PERIOD")]
+    #[arg(value_parser = crate::parser::parse_license_year)]
     pub year: Option<LicenseYear>,
 
-    /// A list of glob patterns to exclude from the licensing process.
+    /// A list of glob patterns to exclude specific files or directories from the licensing process.
     ///
-    /// Defining patterns here is synonymous to adding them either to
-    /// the `.gitignore` or `.licensaignore` file.
-    #[arg(long, value_delimiter = ' ', num_args = 1..)]
+    /// Using this field, you can prevent the application of license headers or other licensing-related
+    /// actions to files that match the specified patterns. This is useful for excluding things like:
+    ///
+    /// - Generated files or third-party code that shouldn't be modified.
+    /// - Files already containing appropriate license information.
+    /// - Files irrelevant to the licensing process.
+    ///
+    ///
+    /// === EXAMPLE USAGE ================================================
+    ///
+    ///     licensa apply --exclude *.txt **/target
+    ///
+    /// === IMPORTANT NOTES ==============================================
+    ///
+    /// - Glob patterns follow standard `.gitignore` patterns.
+    /// - Patterns are case-sensitive.
+    /// - Exclusion applies to files within the workspace or project directory.
+    /// - If a file matches multiple patterns, it's still excluded.
+    /// - Provide multiple patterns as separate space-delimited arguments when using command-line options.
+    #[cfg(not(doctest))]
+    #[arg(long, verbatim_doc_comment)]
+    #[arg(value_name = "GLOB[,...]", value_delimiter = ' ', num_args = 1..)]
     pub exclude: Option<Vec<String>>,
 }
 
@@ -143,6 +163,37 @@ impl Config {
 
         Ok(self.to_owned())
     }
+}
+
+pub struct Copyright {
+    pub license: LicenseId,
+    pub owner: String,
+    pub year: Option<LicenseYear>,
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize, Args)]
+pub struct CopyrightArgs {
+    /// The SPDX identifier of the license (case-insensitive).
+    ///
+    /// SPDX (Software Package Data Exchange) is a standard format for communicating the components,
+    /// licenses, and copyrights associated with software.
+    ///
+    /// See https://spdx.org/licenses/
+    #[arg(short = 't', long = "type", value_parser = crate::parser::parse_license_id)]
+    pub license: Option<LicenseId>,
+
+    /// The copyright owner.
+    #[arg(short, long, value_name = "NAME")]
+    pub owner: Option<String>,
+
+    /// Represents the copyright year or a range of years.
+    ///
+    /// This field is used to define the copyright duration when applying license headers.
+    /// When providing a range, it signifies the inclusive span of years.
+    ///
+    /// The special keyword `present` indicates the current year, e.g. `2022-present`.
+    #[arg(long, value_name = "YYYY | YYYY-YYYY | YYYY-present", value_parser = crate::parser::parse_license_year)]
+    pub year: Option<LicenseYear>,
 }
 
 #[cfg(test)]
